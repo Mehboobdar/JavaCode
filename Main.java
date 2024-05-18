d41af41e7fb0fc6ca09cb9b86ea1c662bd8d727d
mehboobdar <mh5701614@gmail.com>
2024-05-18 14:53:09 +0530

    first code

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.Date;
+
+public class Main {
+
+public static void main(String[] args) {
+
+    System.err.println("hiii");
+
+    Date now =new Date();
+    System.out.println(now);
+    
+}
+
+}
--- mn.java
+++
@@ -1,5 +0,0 @@
-public  class mn {
-
-    int number = 20;
-    String name = "hyder";
-}
