c7beb8b560fbb71a5f1711e279bf6a1d6456b019
mehboobdar <mh5701614@gmail.com>
2024-05-20 18:34:28 +0530

    conditional statements

---
+++ comaprison.java
@@ -0,0 +1,13 @@
+public class comaprison {
+
+    public static void main(String[] args) {
+        int x=10000;
+        if(x>=8000){
+            System.out.println("you are rich");
+        }
+        else if(x<100000){
+        System.out.println("you are pooor");
+        }
+    }
+    
+}
