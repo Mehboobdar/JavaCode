b5249f709406913f906e3b6626693759cf05de59
Mehboob Hussain Dar <117174402+Mehboobdar@users.noreply.github.com>
2024-07-10 14:51:53 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,35 +1,22 @@
-
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.Scanner;
-
-public class Main {
-
-public static void main(String[] args) {
-
-    System.err.println("hiii");
-
-    Date now =new Date();
-    System.out.println(now);
-// array of integers
-   int [] numbers={ 8,9,7,0};
-System.out.println( Arrays.toString(numbers));
-Arrays.sort(numbers);
-System.out.println( Arrays.toString(numbers));
-System.out.println(numbers.length);
-
-Scanner scan=new Scanner( System.in);
-
-
-  
-
-
-
-
-    
-
+class Vehicle {
+    public void start() {
+        System.out.println("Vehicle started");
+    }
 }
 
+class Car extends Vehicle {
+    @Override
+    public void start() {
+        System.out.println("Car started");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Vehicle vehicle = new Vehicle();
+        vehicle.start();
+
+        Car car = new Car();
+        car.start();
+    }
 }
